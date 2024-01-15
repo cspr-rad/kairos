993034a4c9e03651956a2aed090abacf4eb698ec
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2024-01-15 16:28:42 +0100

    Risc0 mock types + serde_json support

---
+++ kairos-prover/kairos-types/src/transfer.rs
@@ -0,0 +1,45 @@
+use casper_types::{account::AccountHash, U512};
+use serde::{Deserialize, Serialize};
+extern crate alloc;
+use alloc::vec::Vec;
+
+pub enum LayerTwoTransaction {
+    Deposit {
+        deploy_status: bool,
+        account: AccountHash,
+        amount: U512,
+    },
+    Withdraw {
+        deploy_status: bool,
+        account: AccountHash,
+        amount: U512,
+    },
+    Transfer {
+        sender: AccountHash,
+        recipient: AccountHash,
+        amount: U512,
+        signature: Vec<u8>, // serialized signature -> replace by struct?,
+                            // pub merkle_proof: MerkleProof -> tbd.
+    },
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct LayerTwoAccount {
+    pub account: AccountHash,
+    pub amount: U512,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct MerkleRoot {
+    pub hash: Vec<u8>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct MockLayerOneState {
+    pub root: MerkleRoot,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct MockLayerTwoState {
+    pub accounts: Vec<LayerTwoAccount>,
+}
--- kairos-prover/methods/guest/src/main.rs
+++ kairos-prover/methods/guest/src/main.rs
@@ -1,11 +1,21 @@
 #![no_main]
-// If you want to try std support, also update the guest Cargo.toml file
-#![no_std] // std support is experimental
-
+#![no_std]
 use risc0_zkvm::guest::env;
-
+use serde_json;
 risc0_zkvm::guest::entry!(main);
+use kairos_types::transfer::{MerkleRoot, MockLayerOneState};
+extern crate alloc;
+use alloc::vec::Vec;
 
 pub fn main() {
-    // TODO: Implement your guest code here
+    let x: u32 = env::read();
+    let y: u32 = env::read();
+    if x != y {
+        panic!("X != Y");
+    }
+    let new_root = MockLayerOneState {
+        root: MerkleRoot { hash: Vec::new() },
+    };
+    let output = &serde_json::to_vec(&new_root).unwrap();
+    env::commit(output);
 }
