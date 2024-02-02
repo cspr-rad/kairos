41ebd06cf51db4ef10eb121cd4c1ac9e743911b3
Andrzej Broński <andrzej1_1@o2.pl>
2024-02-02 12:35:05 +0100

    Run subcommand when matched by name.
    NOTE: If no subcommand was provided, we display help.

--- kairos-cli/bin/main.rs
+++ kairos-cli/bin/main.rs
@@ -3,12 +3,32 @@ mod common;
 
 use clap::Command;
 use commands::{ClientCommand, Deposit, Transfer, Withdraw};
+use std::process;
 
-fn main() {
-    let cli = Command::new("Kairos Client")
+fn cli() -> Command {
+    Command::new("Kairos Client")
         .about("CLI for interacting with Kairos")
         .subcommand(Deposit::new())
         .subcommand(Transfer::new())
-        .subcommand(Withdraw::new());
-    cli.get_matches();
+        .subcommand(Withdraw::new())
+}
+
+fn main() {
+    let arg_matches = cli().get_matches();
+    let (subcommand_name, matches) = arg_matches.subcommand().unwrap_or_else(|| {
+        // No subcommand provided by user.
+        let _ = cli().print_long_help();
+        process::exit(1);
+    });
+
+    match subcommand_name {
+        Deposit::NAME => Deposit::run(matches),
+        Transfer::NAME => Transfer::run(matches),
+        Withdraw::NAME => Withdraw::run(matches),
+        _ => {
+            // This should not happen, unless we missed some subcommand.
+            let _ = cli().print_long_help();
+            process::exit(1);
+        }
+    };
 }
