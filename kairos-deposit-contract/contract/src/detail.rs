df5c3f92f1ae972dbf84a33b780b439ea2a1635f
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2024-02-29 17:55:49 +0100

    add deposit contract crates to workspace

---
+++ kairos-deposit-contract/contract/src/detail.rs
@@ -0,0 +1,62 @@
+use crate::{address::Address, error::Error};
+use casper_contract::{
+    contract_api::{runtime, storage},
+    unwrap_or_revert::UnwrapOrRevert,
+};
+use casper_types::{bytesrepr::FromBytes, system::CallStackElement, ApiError, CLTyped, URef};
+use core::convert::TryInto;
+
+#[allow(dead_code)]
+/// Gets [`URef`] under a name.
+pub(crate) fn get_uref(name: &str) -> URef {
+    let key = runtime::get_key(name)
+        .ok_or(ApiError::MissingKey)
+        .unwrap_or_revert();
+    key.try_into().unwrap_or_revert()
+}
+
+#[allow(dead_code)]
+/// Reads value from a named key.
+pub(crate) fn read_from<T>(name: &str) -> T
+where
+    T: FromBytes + CLTyped,
+{
+    let uref = get_uref(name);
+    let value: T = storage::read(uref).unwrap_or_revert().unwrap_or_revert();
+    value
+}
+
+/// Gets the immediate call stack element of the current execution.
+fn get_immediate_call_stack_item() -> Option<CallStackElement> {
+    let call_stack = runtime::get_call_stack();
+    call_stack.into_iter().rev().nth(1)
+}
+
+/// Returns address based on a [`CallStackElement`].
+///
+/// For `Session` and `StoredSession` variants it will return account hash, and for `StoredContract`
+/// case it will use contract hash as the address.
+fn call_stack_element_to_address(call_stack_element: CallStackElement) -> Address {
+    match call_stack_element {
+        CallStackElement::Session { account_hash } => Address::from(account_hash),
+        CallStackElement::AddressableEntity { package_hash, .. } => Address::from(package_hash),
+    }
+}
+
+#[allow(clippy::manual_next_back)]
+pub(crate) fn get_immediate_caller_address() -> Result<Address, Error> {
+    let call_stack = runtime::get_call_stack();
+    let top_of_the_stack = call_stack
+        .into_iter()
+        .rev()
+        .next()
+        .ok_or(Error::InvalidContext)?;
+    let address = call_stack_element_to_address(top_of_the_stack);
+    Ok(address)
+}
+
+pub(crate) fn get_second_caller_address() -> Result<Address, Error> {
+    let second_from_top_stack_element = get_immediate_call_stack_item().unwrap_or_revert();
+    let address: Address = call_stack_element_to_address(second_from_top_stack_element);
+    Ok(address)
+}
