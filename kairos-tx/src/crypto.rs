d9672c219d6804f1669912246d23ce94eb9b36b1
Andrzej Broński <andrzej1_1@o2.pl>
2024-03-04 12:46:07 +0100

    Allow to skip signature verification with ENV.
    Signature will not be verified if relevant env is set:
    
    ```
    KAIROS_SKIP_SIGNATURE=true
    ```

--- kairos-tx/src/crypto.rs
+++ kairos-tx/src/crypto.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 use crate::error::TxError;
 
 /// This module provides mocked cryptographic functions.
@@ -11,8 +13,22 @@ pub fn sign(data: &[u8], _private_key: &[u8], public_key: &[u8]) -> Vec<u8> {
     [data, public_key].concat()
 }
 
+// Name of ENV variable that can disable signature check.
+// NOTE: This is useful for testing purposes.
+pub const SKIP_SIGNATURE_ENV_VAR: &str = "KAIROS_SKIP_SIGNATURE";
+
 /// Verifies a mock signature against the given data and public key.
+/// If the ENV variable "KAIROS_SKIP_SIGNATURE" is set to "true",
+/// the verification step will be skipped.
 pub fn verify(data: &[u8], signature: &[u8], public_key: &[u8]) -> Result<(), TxError> {
+    // Skip verification if disabled by the environment variable.
+    if env::var(SKIP_SIGNATURE_ENV_VAR)
+        .map(|v| v == "true")
+        .unwrap_or(false)
+    {
+        return Ok(());
+    }
+
     // Re-construct the expected signature using the mock `sign` function logic
     let expected_signature = sign(data, &[], public_key);
 
