3832f74cfbb5a5a5f4f1b7229f1564a54dae99a1
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2024-03-04 02:28:45 +0100

    wip service - add important queries

--- kairos-service/src/main.rs
+++ kairos-service/src/main.rs
@@ -4,7 +4,7 @@ use methods::{
 use serde::{Serialize, Deserialize};
 use risc0_zkvm::{default_prover, ExecutorEnv, Receipt};
 use kairos_risc0_types::{MockLayerTwoStorage, TornadoTree, HashableStruct, TransactionHistory, Transaction, CircuitArgs, CircuitJournal, MockAccounting, ToBytes, Key, U512, hash_bytes};
-use kairos_contract_cli::deployments::get_deposit_event;
+use kairos_contract_cli::deployments::{get_deposit_event, get_counter};
 use std::collections::HashMap;
 
 fn setup_network() -> (TornadoTree, MockLayerTwoStorage){
@@ -43,20 +43,35 @@ fn setup_network() -> (TornadoTree, MockLayerTwoStorage){
     5. Generate proofs for Deposits & Transfers and mutate the L2 state
     6. Submit proofs to the L1
 
+    IDEA: For Demo purpose, trigger the batch submission manually, through a REST endpoint.
+
     7. Implement Transfer signatures (not difficult, but pushed back due to it being a straight-forward process)
 */
 
-fn main(){
+
+async fn await_deposits(){
+    // store L2 index in memory for testing
+    let deposit_index: u128 = 0;
+    loop{
+        let on_chain_height = get_counter::get(node_address, rpc_port, counter_uref).await;
+        // fetch all new L1 deposits
+        if on_chain_height > deposit_index.into(){
+            for i in deposit_index..on_chain_height.as_u128(){
+                // get the deposit and insert it into local storage / apply the L2 balance changes
+                let deposit = get_deposit_event::get(node_address, rpc_port, dict_uref, key).await;
+            }
+        }
+        // add some sort of timeout here
+    }
+}
+
+#[tokio::main]
+async fn main(){
     let state: (TornadoTree, MockLayerTwoStorage) = setup_network();
     /* Storage
         implement a simple storage for 'state' - mysql or even just a file-based I/O script
         add new transactions, update the balances and set the 'processed' flag in storage
     */
-
-
-    // this counter is supposed to live on the blockchain, 
-    // for mock development it can be stored in memory.
-    let deposit_index: u64 = 0;
     // todo: start process that monitors the L1 for Deposits
 
     // todo: accept Transfers
