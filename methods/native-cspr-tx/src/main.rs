2ec09e9f105bba33ada5c105c150c4b08c7734ff
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2024-03-05 19:07:16 +0100

    fix imports

--- methods/native-cspr-tx/src/main.rs
+++ methods/native-cspr-tx/src/main.rs
@@ -4,18 +4,14 @@ risc0_zkvm::guest::entry!(main);
 use kairos_risc0_types::{CircuitArgs, CircuitJournal, HashableStruct, TornadoTree, TransactionBatch, U512};
 
 pub fn main() {
-    let mut inputs: CircuitArgs = env::read();
-    let mut circuit_journal = CircuitJournal{
-        input: inputs.clone(),
-        output: None
-    };
+    let inputs: CircuitArgs = env::read();
     let batch: TransactionBatch = inputs.batch;
     let mut tree: TornadoTree = inputs.tornado;
 
-    // optional: calculate and mutate balance(s)
+/*
     // balances could be committed to the L1 or stored only in the L2
     for transfer in &batch.transfers {
-        //todo: check teh transfer signature
+        //todo: check the transfer signature
     }
 
     // optional: calculate and mutate balance(s)
@@ -26,7 +22,7 @@ pub fn main() {
     for withdrawal in &batch.withdrawals{
 
     }
-
+*/
     // hash the batch and add it to the tornado tree
     let new_leaf: Vec<u8> = batch.hash();
     tree.add_leaf(new_leaf);
