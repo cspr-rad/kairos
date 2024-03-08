5db1a7a7d5b9a21bfbf66c605a688b920c157d27
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2024-03-08 15:16:05 +0100

    submit a transfer to the API

--- api-service/src/main.rs
+++ api-service/src/main.rs
@@ -13,17 +13,22 @@
 */
 use std::convert::Infallible;
 use std::net::SocketAddr;
-use http_body_util::Full;
-use hyper::body::Bytes;
+use http_body_util::{Full, Empty};
+use hyper::body::{Buf, Bytes};
 use hyper::server::conn::http1;
 use hyper::service::service_fn;
 use hyper::{Request, Response};
 use hyper_util::rt::TokioIo;
+use hyper::header;
 use tokio::net::TcpListener;
+use hyper::body::Frame;
+use hyper::{Method, StatusCode};
+use http_body_util::{combinators::BoxBody, BodyExt};
+use kairos_risc0_types::Deposit;
+mod tests;
 
-async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
-}
+//let inputs: models::CircuitInputs = serde_json::from_reader(req.collect().await?.aggregate().reader())?;
+
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
@@ -34,9 +39,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     // We create a TcpListener and bind it to 127.0.0.1:3000
     let listener = TcpListener::bind(addr).await?;
 
+    println!("[Kairos API server] @ {:?}:{}", &host, &port);
     // We start a loop to continuously accept incoming connections
     loop {
-        println!("[Kairos API server] @ {:?}:{}", &host, &port);
         let (stream, _) = listener.accept().await?;
 
         // Use an adapter to access something implementing `tokio::io` traits as if they implement
@@ -48,11 +53,53 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
             // Finally, we bind the incoming connection to our `hello` service
             if let Err(err) = http1::Builder::new()
                 // `service_fn` converts our function in a `Service`
-                .serve_connection(io, service_fn(hello))
+                .serve_connection(io, service_fn(service))
                 .await
             {
                 println!("Error serving connection: {:?}", err);
             }
         });
     }
+}
+
+async fn service(
+    req: Request<hyper::body::Incoming>,
+) -> Result<Response<BoxBody<Bytes, hyper::Error>>, hyper::Error> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => Ok(Response::new(full(
+            "Try submitting to /transfer",
+        ))),
+
+        (&Method::POST, "/transfer") => {
+            let deposit: Deposit = serde_json::from_slice(&req.collect().await.unwrap().to_bytes()).unwrap();
+            // for now this endpoint will simply echo the transfer,
+            // later it should insert the transfer with processed=false into the DB table
+            let response = Response::builder()
+                .status(StatusCode::CREATED)
+                .header(header::CONTENT_TYPE, "application/json")
+                .body(full(serde_json::to_vec(&deposit).unwrap())).unwrap();
+            
+            Ok(response)
+        },
+
+        // Return 404 Not Found for other routes.
+        _ => {
+            let mut not_found = Response::new(empty());
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
+}
+
+// We create some utility functions to make Empty and Full bodies
+// fit our broadened Response body type.
+fn empty() -> BoxBody<Bytes, hyper::Error> {
+    Empty::<Bytes>::new()
+        .map_err(|never| match never {})
+        .boxed()
+}
+fn full<T: Into<Bytes>>(chunk: T) -> BoxBody<Bytes, hyper::Error> {
+    Full::new(chunk.into())
+        .map_err(|never| match never {})
+        .boxed()
 }
