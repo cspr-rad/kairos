5335312f1ea60cc72bda21bd323168b366acf0a7
Andrzej Broński <andrzej1_1@o2.pl>
2024-03-17 17:25:27 +0100

    Use `kairos-crypto` in CLI parsing.

--- kairos-cli/src/commands/transfer.rs
+++ kairos-cli/src/commands/transfer.rs
@@ -1,9 +1,10 @@
 use crate::common::args::{AmountArg, PrivateKeyPathArg};
-use crate::crypto::error::CryptoError;
-use crate::crypto::public_key::CasperPublicKey;
-use crate::crypto::signer::CasperSigner;
 use crate::error::CliError;
 use crate::utils::parse_hex_string;
+
+use kairos_crypto::error::CryptoError;
+use kairos_crypto::implementations::Signer;
+use kairos_crypto::CryptoSigner;
 
 use clap::Parser;
 
@@ -18,10 +19,10 @@ pub struct Args {
 }
 
 pub fn run(args: Args) -> Result<String, CliError> {
-    let _recipient = CasperPublicKey::from_bytes(args.recipient.as_ref())?;
+    let _recipient = Signer::from_public_key(args.recipient.as_ref())?.to_public_key()?;
     let _amount: u64 = args.amount.field;
     let _signer =
-        CasperSigner::from_file(args.private_key_path.field).map_err(CryptoError::from)?;
+        Signer::from_private_key_file(args.private_key_path.field).map_err(CryptoError::from)?;
 
     // TODO: Create transaction and sign it with `signer`.
 
