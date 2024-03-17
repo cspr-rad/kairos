f91232f6dd361743944420a9a22f1f65aa7854d9
Andrzej Broński <andrzej1_1@o2.pl>
2024-03-17 16:50:21 +0100

    Define `CryptoSigner`.

--- kairos-crypto/src/lib.rs
+++ kairos-crypto/src/lib.rs
@@ -1,1 +1,19 @@
 pub mod error;
+
+use std::path::Path;
+
+use error::CryptoError;
+
+pub trait CryptoSigner {
+    fn from_private_key_file<P: AsRef<Path>>(file: P) -> Result<Self, CryptoError>
+    where
+        Self: Sized;
+    fn from_public_key(bytes: &[u8]) -> Result<Self, CryptoError>
+    where
+        Self: Sized;
+
+    fn sign(&self, data: &[u8]) -> Result<Vec<u8>, CryptoError>;
+    fn verify(&self, data: &[u8], signature_bytes: &[u8]) -> Result<(), CryptoError>;
+
+    fn to_public_key(&self) -> Result<Vec<u8>, CryptoError>;
+}
