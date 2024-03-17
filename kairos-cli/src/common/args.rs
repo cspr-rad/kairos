ca0ec09acc4f44a7fc809ab6ac90fabd698d67b2
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-03-17 16:27:59 -0500

    kairos-cli: fix use of deprecated function
    `#[arg(name)] was allowed by mistake, instead use `#[arg(id)]` or `#[arg(value_name)]`

--- kairos-cli/src/common/args.rs
+++ kairos-cli/src/common/args.rs
@@ -4,12 +4,12 @@ use clap::Args;
 
 #[derive(Args, Debug)]
 pub struct AmountArg {
-    #[arg(name = "amount", long, short, value_name = "NUM_MOTES")]
+    #[arg(id = "amount", long, short, value_name = "NUM_MOTES")]
     pub field: u64,
 }
 
 #[derive(Args, Debug)]
 pub struct PrivateKeyPathArg {
-    #[arg(name = "private-key", long, short = 'k', value_name = "FILE_PATH")]
+    #[arg(id = "private-key", long, short = 'k', value_name = "FILE_PATH")]
     pub field: PathBuf,
 }
