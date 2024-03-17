b5b04fe073abe66bce110df8f5f9ea76f28e948c
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-03-17 18:04:48 -0500

    flake/kairos: add frameworks.SystemConfiguration if darwin build

--- flake.nix
+++ flake.nix
@@ -61,6 +61,7 @@
             ] ++ lib.optionals stdenv.isDarwin [
               libiconv
               darwin.apple_sdk.frameworks.Security
+              darwin.apple_sdk.frameworks.SystemConfiguration
             ];
             checkInputs = [
               inputs'.csprpkgs.packages.cctl
