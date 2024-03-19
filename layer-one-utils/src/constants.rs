ce590474c331bbbb1bd9ca8d285dea0d2fd06933
jonas089 <jonas089@gmail.com>
2024-03-19 16:38:53 +0100

    fix l1 client

--- layer-one-utils/src/constants.rs
+++ layer-one-utils/src/constants.rs
@@ -1,12 +1,12 @@
-pub const FORMATTED_COUNTER_UREF: &str = "uref-";
-pub const FORMATTED_DEPOSIT_EVENT_DICT_UREF: &str = "uref-";
-pub const FORMATTED_PROOF_DICT_UREF: &str = "uref-";
+pub const FORMATTED_COUNTER_UREF: &str = "uref-51d404a1981910525d1ab176571ddf9a40bebaf5c389bfca4ed3a9d7feb480ac-007";
+pub const FORMATTED_DEPOSIT_EVENT_DICT_UREF: &str = "uref-99208b0fb75aa18a0e0271268aa34252cec57b5687a3e17a3351c4cfb804a7ad-007";
+pub const FORMATTED_PROOF_DICT_UREF: &str = "uref-4cfd38bafb384a4e284cad6c45c9c76023d7ead6d85bc1fe99430089ea6030e5-007";
 
-pub const DEFAULT_CHAIN_NAME: &str = "cspr-cctl-dev";
+pub const DEFAULT_CHAIN_NAME: &str = "cspr-dev-cctl";
 pub const CCTL_DEFAULT_NODE_ADDRESS: &str = "http://127.0.0.1:11101/rpc";
 pub const CCTL_DEFAULT_NODE_RPC_PORT: &str = "11101";
 
-pub const VERIFIER_CONTRACT_HASH: &str = "contract-";
-pub const SECRET_KEY_PATH: &str = "/home/chef/";
+pub const VERIFIER_CONTRACT_HASH: &str = "contract-bf9e5a19b31e10e5e2a54f99e4adc9d495897199d7f52e811823a22a711205a6";
+pub const SECRET_KEY_PATH: &str = "/home/chef/Desktop/kairos-cctl-env/secret_key.pem";
 
 pub const DEFAULT_PAYMENT_AMOUNT: u64 = 1000_000_000_000;
--- layer-one-utils/src/deployments/query.rs
+++ layer-one-utils/src/deployments/query.rs
@@ -1,42 +1,53 @@
-use casper_client::{get_state_root_hash, query_global_state, get_dictionary_item, JsonRpcId, Verbosity, types::StoredValue};
-use casper_types::{bytesrepr::{Bytes, ToBytes}, CLValue, Key, URef};
+use crate::constants::{
+    CCTL_DEFAULT_NODE_ADDRESS, CCTL_DEFAULT_NODE_RPC_PORT, FORMATTED_COUNTER_UREF,
+    FORMATTED_DEPOSIT_EVENT_DICT_UREF, FORMATTED_PROOF_DICT_UREF,
+};
+use casper_client::{
+    get_dictionary_item, get_state_root_hash, query_global_state, types::StoredValue, JsonRpcId,
+    Verbosity,
+};
 use casper_hashing::Digest;
-use crate::constants::{CCTL_DEFAULT_NODE_ADDRESS, CCTL_DEFAULT_NODE_RPC_PORT, FORMATTED_COUNTER_UREF, FORMATTED_DEPOSIT_EVENT_DICT_UREF, FORMATTED_PROOF_DICT_UREF};
+use casper_types::{
+    bytesrepr::{Bytes, ToBytes},
+    CLValue, Key, URef,
+};
 use kairos_risc0_types::{Deposit, KairosDeltaTree, RiscZeroProof};
 use serde_json;
 
-pub async fn query_state_root_hash(
-    node_address: &str,
-    rpc_port: &str
-) -> Digest{
+pub async fn query_state_root_hash(node_address: &str, rpc_port: &str) -> Digest {
     let srh = get_state_root_hash(
-        JsonRpcId::String(rpc_port.to_owned()), 
-        node_address, 
-        Verbosity::Low, 
-        None
-    ).await.unwrap().result.state_root_hash.unwrap();
+        JsonRpcId::String(rpc_port.to_owned()),
+        node_address,
+        Verbosity::Low,
+        None,
+    )
+    .await
+    .unwrap()
+    .result
+    .state_root_hash
+    .unwrap();
     srh
 }
 
-pub async fn query_counter(
-    node_address: &str,
-    rpc_port: &str,
-    counter_uref: URef
-) -> u64{
+pub async fn query_counter(node_address: &str, rpc_port: &str, counter_uref: URef) -> u64 {
     let srh: Digest = query_state_root_hash(node_address, rpc_port).await;
     let stored_value: StoredValue = query_global_state(
-        JsonRpcId::String(rpc_port.to_owned()), 
-        node_address, 
-        Verbosity::Low, 
-        Some(casper_client::rpcs::GlobalStateIdentifier::StateRootHash(srh)), 
-        casper_types::Key::URef(counter_uref), 
-        Vec::new()
-    ).await.unwrap().result.stored_value;
-    let value: u64 = match stored_value{
-        StoredValue::CLValue(cl_value) => {
-            return cl_value.into_t().unwrap()
-        },
-        _ => return 666u64
+        JsonRpcId::String(rpc_port.to_owned()),
+        node_address,
+        Verbosity::Low,
+        Some(casper_client::rpcs::GlobalStateIdentifier::StateRootHash(
+            srh,
+        )),
+        casper_types::Key::URef(counter_uref),
+        Vec::new(),
+    )
+    .await
+    .unwrap()
+    .result
+    .stored_value;
+    let value: u64 = match stored_value {
+        StoredValue::CLValue(cl_value) => return cl_value.into_t().unwrap(),
+        _ => return 999999999999u64,
     };
 }
 
@@ -44,27 +55,27 @@ pub async fn query_deposit(
     node_address: &str,
     rpc_port: &str,
     dict_uref: URef,
-    key: String
-) -> Deposit{
+    key: String,
+) -> Deposit {
     let srh = query_state_root_hash(node_address, &rpc_port).await;
     let item = get_dictionary_item(
-        JsonRpcId::String(rpc_port.to_owned()), 
-        node_address, 
-        Verbosity::Low, 
-        srh, 
-        casper_client::rpcs::DictionaryItemIdentifier::URef { 
-            seed_uref: dict_uref, 
-            dictionary_item_key: key 
-        }
-    ).await.unwrap();
-    let stored_value: StoredValue = item.result.stored_value;
-    let deposit_bytes: Vec<u8> = match stored_value{
-        StoredValue::CLValue(cl_value) => {
-            cl_value.into_t().unwrap()
+        JsonRpcId::String(rpc_port.to_owned()),
+        node_address,
+        Verbosity::Low,
+        srh,
+        casper_client::rpcs::DictionaryItemIdentifier::URef {
+            seed_uref: dict_uref,
+            dictionary_item_key: key,
         },
-        _ => vec![]
+    )
+    .await
+    .unwrap();
+    let stored_value: StoredValue = item.result.stored_value;
+    let deposit_bytes: Bytes = match stored_value {
+        StoredValue::CLValue(cl_value) => cl_value.into_t().unwrap(),
+        _ => Bytes::from(vec![]),
     };
-    let deposit_deserialized: Deposit = serde_json::from_slice(&deposit_bytes).unwrap();
+    let deposit_deserialized: Deposit = serde_json::from_slice(deposit_bytes.as_slice()).unwrap();
     deposit_deserialized
 }
 
@@ -72,44 +83,62 @@ pub async fn query_proof(
     node_address: &str,
     rpc_port: &str,
     dict_uref: URef,
-    key: String
-) -> RiscZeroProof{
+    key: String,
+) -> RiscZeroProof {
     let srh = query_state_root_hash(node_address, &rpc_port).await;
     let item = get_dictionary_item(
-        JsonRpcId::String(rpc_port.to_owned()), 
-        node_address, 
-        Verbosity::Low, 
-        srh, 
-        casper_client::rpcs::DictionaryItemIdentifier::URef { 
-            seed_uref: dict_uref, 
-            dictionary_item_key: key 
-        }
-    ).await.unwrap();
-    let stored_value: StoredValue = item.result.stored_value;
-    let proof_bytes: Vec<u8> = match stored_value{
-        StoredValue::CLValue(cl_value) => {
-            cl_value.into_t().unwrap()
+        JsonRpcId::String(rpc_port.to_owned()),
+        node_address,
+        Verbosity::Low,
+        srh,
+        casper_client::rpcs::DictionaryItemIdentifier::URef {
+            seed_uref: dict_uref,
+            dictionary_item_key: key,
         },
-        _ => vec![]
+    )
+    .await
+    .unwrap();
+    let stored_value: StoredValue = item.result.stored_value;
+    let proof_bytes: Bytes = match stored_value {
+        StoredValue::CLValue(cl_value) => cl_value.into_t().unwrap(),
+        _ => Bytes::from(vec![]),
     };
-    let proof_deserialized: RiscZeroProof = serde_json::from_slice(&proof_bytes).unwrap();
+    let proof_deserialized: RiscZeroProof = bincode::deserialize(proof_bytes.as_slice()).unwrap();
     proof_deserialized
 }
 
 #[tokio::test]
-async fn test_query_counter(){
-    let response = query_counter(CCTL_DEFAULT_NODE_ADDRESS, CCTL_DEFAULT_NODE_RPC_PORT, URef::from_formatted_str(FORMATTED_COUNTER_UREF).unwrap()).await;
+async fn test_query_counter() {
+    let response = query_counter(
+        CCTL_DEFAULT_NODE_ADDRESS,
+        CCTL_DEFAULT_NODE_RPC_PORT,
+        URef::from_formatted_str(FORMATTED_COUNTER_UREF).unwrap(),
+    )
+    .await;
     println!("Response: {:?}", &response);
 }
 
 #[tokio::test]
-async fn test_query_deposit(){
+async fn test_query_deposit() {
     // query the deposit at index 0
-    let response = query_deposit(CCTL_DEFAULT_NODE_ADDRESS, CCTL_DEFAULT_NODE_RPC_PORT, URef::from_formatted_str(FORMATTED_DEPOSIT_EVENT_DICT_UREF).unwrap(), "0".to_string()).await;
+    let response = query_deposit(
+        CCTL_DEFAULT_NODE_ADDRESS,
+        CCTL_DEFAULT_NODE_RPC_PORT,
+        URef::from_formatted_str(FORMATTED_DEPOSIT_EVENT_DICT_UREF).unwrap(),
+        "0".to_string(),
+    )
+    .await;
     println!("Response: {:?}", &response);
 }
 
 #[tokio::test]
-async fn test_query_proof(){
-    let response = query_proof(CCTL_DEFAULT_NODE_ADDRESS, CCTL_DEFAULT_NODE_RPC_PORT, URef::from_formatted_str(FORMATTED_PROOF_DICT_UREF).unwrap(), "0".to_string()).await;
+async fn test_query_proof() {
+    let response = query_proof(
+        CCTL_DEFAULT_NODE_ADDRESS,
+        CCTL_DEFAULT_NODE_RPC_PORT,
+        URef::from_formatted_str(FORMATTED_PROOF_DICT_UREF).unwrap(),
+        "0".to_string(),
+    )
+    .await;
+    println!("Response: {:?}", &response);
 }
