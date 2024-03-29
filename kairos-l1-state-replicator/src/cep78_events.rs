95b45d69ab68d76002501632cc394387a213dd04
Andrzej Broński <andrzej1_1@o2.pl>
2024-03-29 01:01:29 +0100

    Reduce derives in CEP78 events.

--- kairos-l1-state-replicator/src/cep78_events.rs
+++ kairos-l1-state-replicator/src/cep78_events.rs
@@ -1,46 +1,46 @@
 use casper_event_standard::Event;
 use casper_types::Key;
 
-#[derive(Event, Debug, PartialEq, Eq)]
+#[derive(Event, Debug)]
 pub struct Mint {
     recipient: Key,
     token_id: String,
     data: String,
 }
 
-#[derive(Event, Debug, PartialEq, Eq)]
+#[derive(Event, Debug)]
 pub struct Burn {
     owner: Key,
     token_id: String,
     burner: Key,
 }
 
-#[derive(Event, Debug, PartialEq, Eq)]
+#[derive(Event, Debug)]
 pub struct Approval {
     owner: Key,
     spender: Key,
     token_id: String,
 }
 
-#[derive(Event, Debug, PartialEq, Eq)]
+#[derive(Event, Debug)]
 pub struct ApprovalRevoked {
     owner: Key,
     token_id: String,
 }
 
-#[derive(Event, Debug, PartialEq, Eq)]
+#[derive(Event, Debug)]
 pub struct ApprovalForAll {
     owner: Key,
     operator: Key,
 }
 
-#[derive(Event, Debug, PartialEq, Eq)]
+#[derive(Event, Debug)]
 pub struct RevokedForAll {
     owner: Key,
     operator: Key,
 }
 
-#[derive(Event, Debug, PartialEq, Eq)]
+#[derive(Event, Debug)]
 pub struct Transfer {
     owner: Key,
     spender: Option<Key>,
@@ -48,14 +48,14 @@ pub struct Transfer {
     token_id: String,
 }
 
-#[derive(Event, Debug, PartialEq, Eq)]
+#[derive(Event, Debug)]
 pub struct MetadataUpdated {
     token_id: String,
     data: String,
 }
 
-#[derive(Event, Debug, PartialEq, Eq, Default)]
+#[derive(Event, Debug)]
 pub struct VariablesSet {}
 
-#[derive(Event, Debug, PartialEq, Eq, Default)]
+#[derive(Event, Debug)]
 pub struct Migration {}
