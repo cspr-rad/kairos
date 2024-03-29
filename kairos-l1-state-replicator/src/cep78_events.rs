0035111c529a1a6df77aacc42e7105e069b138c4
Andrzej Broński <andrzej1_1@o2.pl>
2024-03-29 01:07:01 +0100

    Update events order + add comment.

--- kairos-l1-state-replicator/src/cep78_events.rs
+++ kairos-l1-state-replicator/src/cep78_events.rs
@@ -1,30 +1,13 @@
+/// CEP-78 events defined with Casper Event Standard.
+/// Source: https://github.com/casper-ecosystem/cep-78-enhanced-nft/blob/7815f090b51b9153dd33a3d7c0ab939b61e5a411/contract/src/events/events_ces.rs
+///
 use casper_event_standard::Event;
 use casper_types::Key;
-
-#[derive(Event, Debug)]
-pub struct Mint {
-    recipient: Key,
-    token_id: String,
-    data: String,
-}
-
-#[derive(Event, Debug)]
-pub struct Burn {
-    owner: Key,
-    token_id: String,
-    burner: Key,
-}
 
 #[derive(Event, Debug)]
 pub struct Approval {
     owner: Key,
     spender: Key,
-    token_id: String,
-}
-
-#[derive(Event, Debug)]
-pub struct ApprovalRevoked {
-    owner: Key,
     token_id: String,
 }
 
@@ -35,9 +18,32 @@ pub struct ApprovalForAll {
 }
 
 #[derive(Event, Debug)]
-pub struct RevokedForAll {
+pub struct ApprovalRevoked {
     owner: Key,
-    operator: Key,
+    token_id: String,
+}
+
+#[derive(Event, Debug)]
+pub struct Burn {
+    owner: Key,
+    token_id: String,
+    burner: Key,
+}
+
+#[derive(Event, Debug)]
+pub struct MetadataUpdated {
+    token_id: String,
+    data: String,
+}
+
+#[derive(Event, Debug)]
+pub struct Migration {}
+
+#[derive(Event, Debug)]
+pub struct Mint {
+    recipient: Key,
+    token_id: String,
+    data: String,
 }
 
 #[derive(Event, Debug)]
@@ -49,13 +55,10 @@ pub struct Transfer {
 }
 
 #[derive(Event, Debug)]
-pub struct MetadataUpdated {
-    token_id: String,
-    data: String,
-}
-
-#[derive(Event, Debug)]
 pub struct VariablesSet {}
 
 #[derive(Event, Debug)]
-pub struct Migration {}
+pub struct RevokedForAll {
+    owner: Key,
+    operator: Key,
+}
