2145016eb382682b3e9e6d9555ff05819931178e
Andrzej Broński <andrzej1_1@o2.pl>
2024-04-20 15:36:34 +0200

    Fix formatting.

--- casper-event-toolkit/src/event.rs
+++ casper-event-toolkit/src/event.rs
@@ -1,6 +1,9 @@
 use casper_event_standard::Schema;
 use casper_types::{
-    bytesrepr::{FromBytes, ToBytes, OPTION_NONE_TAG, OPTION_SOME_TAG, RESULT_ERR_TAG, RESULT_OK_TAG}, CLType, CLValue
+    bytesrepr::{
+        FromBytes, ToBytes, OPTION_NONE_TAG, OPTION_SOME_TAG, RESULT_ERR_TAG, RESULT_OK_TAG,
+    },
+    CLType, CLValue,
 };
 
 use crate::error::ToolkitError;
@@ -42,7 +45,6 @@ pub fn parse_dynamic_event_data(
     let mut remainder = event_data;
     let schema_fields = dynamic_event_schema.to_vec();
     for (field_name, field_type) in schema_fields {
-
         let cltype = field_type.downcast();
         let (field_value, new_remainder) = parse_dynamic_clvalue(&cltype, remainder)?;
         remainder = new_remainder;
@@ -62,78 +64,123 @@ pub fn parse_dynamic_event_data(
 // - deserializing data - based on `from_bytes()`,
 // - serializing data - based on `to_bytes()`.
 //
-fn parse_dynamic_clvalue<'a>(cltype: &CLType, bytes: &'a [u8]) -> Result<(CLValue, &'a [u8]), ToolkitError> {
+fn parse_dynamic_clvalue<'a>(
+    cltype: &CLType,
+    bytes: &'a [u8],
+) -> Result<(CLValue, &'a [u8]), ToolkitError> {
     let result = match cltype {
         casper_types::CLType::Bool => {
             let (value, new_remainder) = bool::from_bytes(bytes).unwrap();
             let value_bytes = value.to_bytes().unwrap();
-            (CLValue::from_components(casper_types::CLType::Bool, value_bytes), new_remainder)
-        },
+            (
+                CLValue::from_components(casper_types::CLType::Bool, value_bytes),
+                new_remainder,
+            )
+        }
         casper_types::CLType::I32 => {
             let (value, new_remainder) = i32::from_bytes(bytes).unwrap();
             let value_bytes = value.to_bytes().unwrap();
-            (CLValue::from_components(casper_types::CLType::I32, value_bytes), new_remainder)
-        },
+            (
+                CLValue::from_components(casper_types::CLType::I32, value_bytes),
+                new_remainder,
+            )
+        }
         casper_types::CLType::I64 => {
             let (value, new_remainder) = i64::from_bytes(bytes).unwrap();
             let value_bytes = value.to_bytes().unwrap();
-            (CLValue::from_components(casper_types::CLType::I64, value_bytes), new_remainder)
-        },
+            (
+                CLValue::from_components(casper_types::CLType::I64, value_bytes),
+                new_remainder,
+            )
+        }
         casper_types::CLType::U8 => {
             let (value, new_remainder) = u8::from_bytes(bytes).unwrap();
             let value_bytes = value.to_bytes().unwrap();
-            (CLValue::from_components(casper_types::CLType::U8, value_bytes), new_remainder)
-        },
+            (
+                CLValue::from_components(casper_types::CLType::U8, value_bytes),
+                new_remainder,
+            )
+        }
         casper_types::CLType::U32 => {
             let (value, new_remainder) = u32::from_bytes(bytes).unwrap();
             let value_bytes = value.to_bytes().unwrap();
-            (CLValue::from_components(casper_types::CLType::U32, value_bytes), new_remainder)
-        },
+            (
+                CLValue::from_components(casper_types::CLType::U32, value_bytes),
+                new_remainder,
+            )
+        }
         casper_types::CLType::U64 => {
             let (value, new_remainder) = u64::from_bytes(bytes).unwrap();
             let value_bytes = value.to_bytes().unwrap();
-            (CLValue::from_components(casper_types::CLType::U64, value_bytes), new_remainder)
-        },
+            (
+                CLValue::from_components(casper_types::CLType::U64, value_bytes),
+                new_remainder,
+            )
+        }
         casper_types::CLType::U128 => {
             let (value, new_remainder) = casper_types::U128::from_bytes(bytes).unwrap();
             let value_bytes = value.to_bytes().unwrap();
-            (CLValue::from_components(casper_types::CLType::U128, value_bytes), new_remainder)
-        },
+            (
+                CLValue::from_components(casper_types::CLType::U128, value_bytes),
+                new_remainder,
+            )
+        }
         casper_types::CLType::U256 => {
             let (value, new_remainder) = casper_types::U256::from_bytes(bytes).unwrap();
             let value_bytes = value.to_bytes().unwrap();
-            (CLValue::from_components(casper_types::CLType::U256, value_bytes), new_remainder)
-        },
+            (
+                CLValue::from_components(casper_types::CLType::U256, value_bytes),
+                new_remainder,
+            )
+        }
         casper_types::CLType::U512 => {
             let (value, new_remainder) = casper_types::U512::from_bytes(bytes).unwrap();
             let value_bytes = value.to_bytes().unwrap();
-            (CLValue::from_components(casper_types::CLType::U512, value_bytes), new_remainder)
-        },
+            (
+                CLValue::from_components(casper_types::CLType::U512, value_bytes),
+                new_remainder,
+            )
+        }
         casper_types::CLType::Unit => {
             let (value, new_remainder) = <()>::from_bytes(bytes).unwrap();
             let value_bytes = value.to_bytes().unwrap();
-            (CLValue::from_components(casper_types::CLType::Unit, value_bytes), new_remainder)
-        },
+            (
+                CLValue::from_components(casper_types::CLType::Unit, value_bytes),
+                new_remainder,
+            )
+        }
         casper_types::CLType::String => {
             let (value, new_remainder) = String::from_bytes(bytes).unwrap();
             let value_bytes = value.to_bytes().unwrap();
-            (CLValue::from_components(casper_types::CLType::String, value_bytes), new_remainder)
+            (
+                CLValue::from_components(casper_types::CLType::String, value_bytes),
+                new_remainder,
+            )
         }
         casper_types::CLType::Key => {
             let (value, new_remainder) = casper_types::Key::from_bytes(bytes).unwrap();
             let value_bytes = value.to_bytes().unwrap();
-            (CLValue::from_components(casper_types::CLType::Key, value_bytes), new_remainder)
+            (
+                CLValue::from_components(casper_types::CLType::Key, value_bytes),
+                new_remainder,
+            )
         }
         casper_types::CLType::URef => {
             let (value, new_remainder) = casper_types::URef::from_bytes(bytes).unwrap();
             let value_bytes = value.to_bytes().unwrap();
-            (CLValue::from_components(casper_types::CLType::URef, value_bytes), new_remainder)
-        },
+            (
+                CLValue::from_components(casper_types::CLType::URef, value_bytes),
+                new_remainder,
+            )
+        }
         casper_types::CLType::PublicKey => {
             let (value, new_remainder) = casper_types::PublicKey::from_bytes(bytes).unwrap();
             let value_bytes = value.to_bytes().unwrap();
-            (CLValue::from_components(casper_types::CLType::PublicKey, value_bytes), new_remainder)
-        },
+            (
+                CLValue::from_components(casper_types::CLType::PublicKey, value_bytes),
+                new_remainder,
+            )
+        }
         // More complex types.
         casper_types::CLType::Option(t) => {
             let (tag, remainder) = u8::from_bytes(bytes).unwrap();
@@ -141,17 +188,29 @@ fn parse_dynamic_clvalue<'a>(cltype: &CLType, bytes: &'a [u8]) -> Result<(CLValu
                 OPTION_NONE_TAG => {
                     let value_bytes = vec![tag];
                     let new_remainder = remainder;
-                    (CLValue::from_components(casper_types::CLType::Option(t.clone()), value_bytes), new_remainder)
+                    (
+                        CLValue::from_components(
+                            casper_types::CLType::Option(t.clone()),
+                            value_bytes,
+                        ),
+                        new_remainder,
+                    )
                 }
                 OPTION_SOME_TAG => {
                     let (t_parsed, new_remainder) = parse_dynamic_clvalue(t, remainder).unwrap();
                     let mut value_bytes = vec![tag];
                     value_bytes.extend(t_parsed.inner_bytes());
-                    (CLValue::from_components(casper_types::CLType::Option(t.clone()), value_bytes), new_remainder)
+                    (
+                        CLValue::from_components(
+                            casper_types::CLType::Option(t.clone()),
+                            value_bytes,
+                        ),
+                        new_remainder,
+                    )
                 }
                 _ => panic!("Err(Error::Formatting)"),
             }
-        },
+        }
         casper_types::CLType::List(t) => {
             let (count, mut remainder) = u32::from_bytes(bytes).unwrap();
             let mut value_bytes = vec![];
@@ -161,60 +220,111 @@ fn parse_dynamic_clvalue<'a>(cltype: &CLType, bytes: &'a [u8]) -> Result<(CLValu
                 remainder = next_remainder;
                 value_bytes.extend(t1_parsed.inner_bytes());
             }
-            (CLValue::from_components(casper_types::CLType::List(t.clone()), value_bytes), remainder)
-        },
+            (
+                CLValue::from_components(casper_types::CLType::List(t.clone()), value_bytes),
+                remainder,
+            )
+        }
         casper_types::CLType::ByteArray(t) => {
             let fixed_length = *t as usize;
             let mut value_bytes = vec![];
             let (t_parsed, new_remainder) = bytes.split_at(fixed_length);
             value_bytes.extend(t_parsed);
-            (CLValue::from_components(casper_types::CLType::ByteArray(t.clone()), value_bytes), new_remainder)
-        },
-        casper_types::CLType::Result { ok: t_ok, err: t_err } => {
+            (
+                CLValue::from_components(casper_types::CLType::ByteArray(t.clone()), value_bytes),
+                new_remainder,
+            )
+        }
+        casper_types::CLType::Result {
+            ok: t_ok,
+            err: t_err,
+        } => {
             let (variant, remainder) = u8::from_bytes(bytes).unwrap();
             match variant {
                 RESULT_ERR_TAG => {
-                    let (t_err_parsed, new_remainder) = parse_dynamic_clvalue(t_err, remainder).unwrap();
+                    let (t_err_parsed, new_remainder) =
+                        parse_dynamic_clvalue(t_err, remainder).unwrap();
                     let mut value_bytes = vec![variant];
                     value_bytes.extend(t_err_parsed.inner_bytes());
-                    (CLValue::from_components(casper_types::CLType::Result { ok: t_ok.clone(), err: t_err.clone() }, value_bytes), new_remainder)
-                },
+                    (
+                        CLValue::from_components(
+                            casper_types::CLType::Result {
+                                ok: t_ok.clone(),
+                                err: t_err.clone(),
+                            },
+                            value_bytes,
+                        ),
+                        new_remainder,
+                    )
+                }
                 RESULT_OK_TAG => {
-                    let (t_ok_parsed, new_remainder) = parse_dynamic_clvalue(t_ok, remainder).unwrap();
+                    let (t_ok_parsed, new_remainder) =
+                        parse_dynamic_clvalue(t_ok, remainder).unwrap();
                     let mut value_bytes = vec![variant];
                     value_bytes.extend(t_ok_parsed.inner_bytes());
-                    (CLValue::from_components(casper_types::CLType::Result { ok: t_ok.clone(), err: t_err.clone() }, value_bytes), new_remainder)
-                },
+                    (
+                        CLValue::from_components(
+                            casper_types::CLType::Result {
+                                ok: t_ok.clone(),
+                                err: t_err.clone(),
+                            },
+                            value_bytes,
+                        ),
+                        new_remainder,
+                    )
+                }
                 _ => panic!("Err(Error::Formatting)"),
             }
-        },
-        casper_types::CLType::Map { key: t_key, value: t_value } => {
+        }
+        casper_types::CLType::Map {
+            key: t_key,
+            value: t_value,
+        } => {
             let (num_keys, mut remainder) = u32::from_bytes(bytes).unwrap();
             let mut value_bytes = vec![];
             value_bytes.extend(num_keys.to_bytes().unwrap());
             for _ in 0..num_keys {
                 let (key_parsed, next_remainder) = parse_dynamic_clvalue(t_key, remainder).unwrap();
-                let (value_parsed, next_remainder) = parse_dynamic_clvalue(t_value, next_remainder).unwrap();
+                let (value_parsed, next_remainder) =
+                    parse_dynamic_clvalue(t_value, next_remainder).unwrap();
                 remainder = next_remainder;
                 value_bytes.extend(key_parsed.inner_bytes());
                 value_bytes.extend(value_parsed.inner_bytes());
             }
-            (CLValue::from_components(casper_types::CLType::Map { key: t_key.clone(), value: t_value.clone() }, value_bytes), remainder)
-        },
+            (
+                CLValue::from_components(
+                    casper_types::CLType::Map {
+                        key: t_key.clone(),
+                        value: t_value.clone(),
+                    },
+                    value_bytes,
+                ),
+                remainder,
+            )
+        }
         casper_types::CLType::Tuple1([t1]) => {
             let (t1_parsed, new_remainder) = parse_dynamic_clvalue(t1, bytes).unwrap();
             let mut value_bytes = vec![];
             value_bytes.extend(t1_parsed.inner_bytes());
-            (CLValue::from_components(casper_types::CLType::Tuple1([t1.clone()]), value_bytes), new_remainder)
-        },
+            (
+                CLValue::from_components(casper_types::CLType::Tuple1([t1.clone()]), value_bytes),
+                new_remainder,
+            )
+        }
         casper_types::CLType::Tuple2([t1, t2]) => {
             let (t1_parsed, remainder) = parse_dynamic_clvalue(t1, bytes).unwrap();
             let (t2_parsed, new_remainder) = parse_dynamic_clvalue(t2, remainder).unwrap();
             let mut value_bytes = vec![];
             value_bytes.extend(t1_parsed.inner_bytes());
             value_bytes.extend(t2_parsed.inner_bytes());
-            (CLValue::from_components(casper_types::CLType::Tuple2([t1.clone(), t2.clone()]), value_bytes), new_remainder)
-        },
+            (
+                CLValue::from_components(
+                    casper_types::CLType::Tuple2([t1.clone(), t2.clone()]),
+                    value_bytes,
+                ),
+                new_remainder,
+            )
+        }
         casper_types::CLType::Tuple3([t1, t2, t3]) => {
             let (t1_parsed, remainder) = parse_dynamic_clvalue(t1, bytes).unwrap();
             let (t2_parsed, remainder) = parse_dynamic_clvalue(t2, remainder).unwrap();
@@ -223,15 +333,24 @@ fn parse_dynamic_clvalue<'a>(cltype: &CLType, bytes: &'a [u8]) -> Result<(CLValu
             value_bytes.extend(t1_parsed.inner_bytes());
             value_bytes.extend(t2_parsed.inner_bytes());
             value_bytes.extend(t3_parsed.inner_bytes());
-            (CLValue::from_components(casper_types::CLType::Tuple3([t1.clone(), t2.clone(), t3.clone()]), value_bytes), new_remainder)
-        },
+            (
+                CLValue::from_components(
+                    casper_types::CLType::Tuple3([t1.clone(), t2.clone(), t3.clone()]),
+                    value_bytes,
+                ),
+                new_remainder,
+            )
+        }
         casper_types::CLType::Any => {
             // Consume all the remaining bytes and put it in `Any` type.
             let (new_remainder, value_bytes) = bytes.split_at(0);
-            (CLValue::from_components(casper_types::CLType::Any, value_bytes.to_vec()), new_remainder)
-        },
+            (
+                CLValue::from_components(casper_types::CLType::Any, value_bytes.to_vec()),
+                new_remainder,
+            )
+        }
     };
-    
+
     Ok(result)
 }
 
@@ -257,7 +376,10 @@ mod tests {
         let (parsed_clvalue, remainder) = parse_dynamic_clvalue(cltype, bytes).unwrap();
 
         // Asserts.
-        assert_eq!(parsed_clvalue, clvalue, "Roundtrip should give the same CLValue.");
+        assert_eq!(
+            parsed_clvalue, clvalue,
+            "Roundtrip should give the same CLValue."
+        );
         assert!(remainder.is_empty(), "All bytes should have been consumed.");
     }
 
