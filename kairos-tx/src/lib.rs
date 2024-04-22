34f3fee549c9ab8b065cb7023500e9ce435814b3
Andrzej Broński <andrzej1_1@o2.pl>
2024-04-22 14:28:45 +0200

    Add constructor for ASN.1 `Transaction`.

--- kairos-tx/src/lib.rs
+++ kairos-tx/src/lib.rs
@@ -4,3 +4,20 @@ extern crate alloc;
 pub mod asn;
 pub mod error;
 pub mod helpers;
+
+// Constructor for non-exhaustive transaction struct.
+pub fn make_tx(
+    public_key: asn::PublicKey,
+    payload: asn::SigningPayload,
+    algorithm: asn::SigningAlgorithm,
+    signature: asn::Signature,
+) -> asn::Transaction {
+    let transaction = asn::Transaction {
+        public_key,
+        payload,
+        algorithm,
+        signature,
+    };
+
+    transaction
+}
