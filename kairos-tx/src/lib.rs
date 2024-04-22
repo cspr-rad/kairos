171e5f91f5eed48209ceb0aa6f92e926ba58137f
Andrzej Broński <andrzej1_1@o2.pl>
2024-04-22 13:15:29 +0200

    Implement transaction hashing.

--- kairos-tx/src/lib.rs
+++ kairos-tx/src/lib.rs
@@ -5,6 +5,16 @@ pub mod asn;
 pub mod error;
 pub mod helpers;
 
+use sha2::Digest;
+
+// Computes the hash for a transaction.
+// Hash is obtained from payload by computing sha256 of DER encoded ASN.1 data.
+pub fn hash(payload: &asn::SigningPayload) -> Result<[u8; 32], error::TxError> {
+    let data = rasn::der::encode(payload).map_err(error::TxError::EncodeError)?;
+    let tx_hash: [u8; 32] = sha2::Sha256::digest(&data).into();
+    Ok(tx_hash)
+}
+
 // Constructor for non-exhaustive transaction struct.
 pub fn make_tx(
     public_key: asn::PublicKey,
