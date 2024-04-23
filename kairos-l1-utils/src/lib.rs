ef1d2c31a2f78149d1b68f87a0cdc3061e2bd1df
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2024-04-23 15:29:21 +0200

    apply clippy suggestions

--- kairos-l1-utils/src/lib.rs
+++ kairos-l1-utils/src/lib.rs
@@ -1,22 +1,19 @@
 use casper_client::{
-    get_dictionary_item, get_state_root_hash, query_global_state, types::StoredValue, JsonRpcId,
+    get_state_root_hash, query_global_state, types::StoredValue, JsonRpcId,
     Verbosity,
 };
 use casper_hashing::Digest;
-use casper_types::{
-    bytesrepr::{Bytes, ToBytes},
-    CLValue, Key, URef,
-};
+use casper_types::URef;
 
 use casper_client::{
     rpcs::results::PutDeployResult,
     types::{Deploy, DeployBuilder, ExecutableDeployItem, Timestamp},
     SuccessResponse,
 };
-use casper_types::{crypto::SecretKey, runtime_args, ContractHash, RuntimeArgs};
-use std::{fs, io::Read, result};
+use casper_types::{crypto::SecretKey, RuntimeArgs};
+use std::fs;
 
-pub const DEFAULT_PAYMENT_AMOUNT: u64 = 1000_000_000_000;
+pub const DEFAULT_PAYMENT_AMOUNT: u64 = 1_000_000_000_000;
 
 pub async fn install_wasm_bytecode(
     node_address: &str,
@@ -37,19 +34,18 @@ pub async fn install_wasm_bytecode(
         .build()
         .unwrap();
 
-    let result = casper_client::put_deploy(
+    casper_client::put_deploy(
         JsonRpcId::String(rpc_port.to_string()),
         node_address,
         casper_client::Verbosity::Low,
         deploy,
     )
     .await
-    .unwrap();
-    result
+    .unwrap()
 }
 
 pub async fn query_state_root_hash(node_address: &str, rpc_port: &str) -> Digest {
-    let srh = get_state_root_hash(
+    get_state_root_hash(
         JsonRpcId::String(rpc_port.to_owned()),
         node_address,
         Verbosity::Low,
@@ -59,8 +55,7 @@ pub async fn query_state_root_hash(node_address: &str, rpc_port: &str) -> Digest
     .unwrap()
     .result
     .state_root_hash
-    .unwrap();
-    srh
+    .unwrap()
 }
 
 pub async fn query_counter(node_address: &str, rpc_port: &str, counter_uref: &str) -> u64 {
@@ -93,6 +88,8 @@ async fn state_root_hash() {
 #[tokio::test]
 async fn install_wasm() {
     use std::fs::File;
+    use std::io::Read;
+    use casper_types::{RuntimeArgs, runtime_args};
     let mut wasm_file: File =
         File::open("/Users/chef/Desktop/demo-contract-optimized.wasm").unwrap();
     let mut wasm_bytes: Vec<u8> = Vec::new();
