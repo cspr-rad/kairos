fa7568ae6cae04d7d8438405473243b348d14609
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-05-03 14:10:46 -0500

    kairos-cli: implement call to deposit endpoint and integration test

--- kairos-cli/src/commands/deposit.rs
+++ kairos-cli/src/commands/deposit.rs
@@ -1,9 +1,14 @@
+use crate::client;
 use crate::common::args::{AmountArg, PrivateKeyPathArg};
 use crate::error::CliError;
+
+use reqwest::Url;
 
 use kairos_crypto::error::CryptoError;
 use kairos_crypto::implementations::Signer;
 use kairos_crypto::CryptoSigner;
+use kairos_server::routes::PayloadBody;
+use kairos_tx::asn::SigningPayload;
 
 use clap::Parser;
 
@@ -15,14 +20,31 @@ pub struct Args {
     private_key_path: PrivateKeyPathArg,
 }
 
-pub fn run(args: Args) -> Result<String, CliError> {
-    let _amount: u64 = args.amount.field;
-    let _signer =
+pub fn run(args: Args, kairos_server_address: Url) -> Result<String, CliError> {
+    let amount: u64 = args.amount.field;
+    let signer =
         Signer::from_private_key_file(args.private_key_path.field).map_err(CryptoError::from)?;
 
-    // TODO: Create transaction and sign it with `signer`.
+    let client = reqwest::Client::new();
+    let public_key = signer.to_public_key()?;
 
-    // TODO: Send transaction to the network, using Rust SDK.
+    let payload = SigningPayload::new_deposit(amount)
+        .try_into()
+        .expect("Failed serialize the deposit payload to bytes");
+    let signature = signer.sign(&payload)?;
+    let deposit_request = PayloadBody {
+        public_key,
+        payload,
+        signature,
+    };
 
-    Ok("ok".to_string())
+    tokio::runtime::Runtime::new()
+        .unwrap()
+        .block_on(client::submit_transaction_request(
+            &client,
+            &kairos_server_address,
+            &deposit_request,
+        ))
+        .map_err(Into::<CliError>::into)
+        .map(|_| "ok".to_string())
 }
--- kairos-cli/src/lib.rs
+++ kairos-cli/src/lib.rs
@@ -3,3 +3,42 @@ pub mod commands;
 pub mod common;
 pub mod error;
 pub mod utils;
+
+use crate::error::CliError;
+
+use clap::{Parser, Subcommand};
+use reqwest::Url;
+
+#[derive(Parser)]
+#[command(name = "Kairos Client", about = "CLI for interacting with Kairos")]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Command,
+    #[arg(long, value_name = "URL")]
+    pub kairos_server_address: Option<Url>,
+}
+
+#[derive(Subcommand)]
+pub enum Command {
+    #[command(about = "Deposits funds into your account")]
+    Deposit(commands::deposit::Args),
+    #[command(about = "Transfers funds to another account")]
+    Transfer(commands::transfer::Args),
+    #[command(about = "Withdraws funds from your account")]
+    Withdraw(commands::withdraw::Args),
+}
+
+pub fn run(
+    Cli {
+        command,
+        kairos_server_address,
+    }: Cli,
+) -> Result<String, CliError> {
+    let kairos_server_address =
+        kairos_server_address.unwrap_or(Url::parse("http://0.0.0.0:9999").unwrap());
+    match command {
+        Command::Deposit(args) => commands::deposit::run(args, kairos_server_address),
+        Command::Transfer(args) => commands::transfer::run(args),
+        Command::Withdraw(args) => commands::withdraw::run(args),
+    }
+}
