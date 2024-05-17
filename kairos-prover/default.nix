105981c078585b5f796aadeb79e90352fe88c0f5
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-05-17 15:44:18 -0400

    kairos-prover: build with crane

--- kairos-prover/default.nix
+++ kairos-prover/default.nix
@@ -1,41 +1,78 @@
 { self, inputs, ... }:
 {
-  perSystem = { config, self', inputs', system, pkgs, lib, ... }:
+  perSystem = { self', inputs', system, pkgs, lib, ... }:
+    let
+      rustToolchain = inputs'.fenix.packages.latest.toolchain;
+      craneLib = inputs.crane.lib.${system}.overrideToolchain rustToolchain;
+
+      rustup-mock = pkgs.writeShellApplication {
+        name = "rustup";
+        text = ''
+          # the buildscript uses rustup toolchain to check
+          # whether the risc0 toolchain was installed
+          if [[ "$1" = "toolchain" ]]
+          then
+            printf "risc0\n"
+          elif [[ "$1" = "+risc0" ]]
+          then
+            printf "${rustToolchain}/bin/rustc"
+          fi
+        '';
+      };
+
+      kairosProverAttrs = rec {
+        src = lib.cleanSourceWith {
+          src = craneLib.path ./.;
+          filter = craneLib.filterCargoSources;
+        };
+        nativeBuildInputs = with pkgs; [
+          pkg-config
+          cargo-risczero
+          rustup-mock
+        ];
+        buildInputs = with pkgs; [
+          openssl.dev
+        ] ++ lib.optionals stdenv.isDarwin [
+          darwin.apple_sdk.frameworks.SystemConfiguration
+        ];
+        cargoVendorDir = inputs.crane.lib.${system}.vendorMultipleCargoDeps {
+          inherit (craneLib.findCargoFiles src) cargoConfigs;
+          cargoLockList = [
+            ./methods/guest/Cargo.lock
+            ./Cargo.lock
+            ./rust-std-Cargo.lock
+          ];
+        };
+        preBuild = ''
+          # The vendored cargo sources will be placed into .cargo-home,
+          # however it seems that since the risc0_build crate
+          # calls cargo at build time in this directory cargo will be
+          # looking for .cargo
+          mkdir .cargo
+          mv .cargo-home/config.toml .cargo/config.toml
+          export RISC0_RUST_SRC=${rustToolchain}/lib/rustlib/src/rust;
+        '';
+      };
+    in
     {
       devShells.risczero = pkgs.mkShell {
-        RISC0_RUST_SRC = "${self'.packages.kairos-prover.toolchain}/lib/rustlib/src/rust";
+        RISC0_RUST_SRC = "${rustToolchain}/lib/rustlib/src/rust";
         RISC0_DEV_MODE = 1;
         inputsFrom = [ self.packages.${system}.kairos-prover ];
         # I cannot install Metal via Nix, so you need to follow the standard xcode metal installation instructions
-        buildInputs = lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [ Metal SystemConfiguration ]);
-        nativeBuildInputs = [ inputs'.risc0pkgs.packages.r0vm ];
+        nativeBuildInputs = [
+          inputs'.risc0pkgs.packages.r0vm
+        ] ++ lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [ Metal SystemConfiguration ]);
       };
       packages = {
-        kairos-prover = inputs.risc0pkgs.lib.${system}.buildRisc0Package {
-          pname = "kairos-prover";
-          version = "0.0.1";
-            src = lib.cleanSourceWith {
-              src = ../.;
-              filter = path: type:
-                (builtins.any (includePath: lib.hasInfix includePath path) [
-                  "/kairos-prover"
-                  "/kairos-tx"
-                  "/kairos-circuit-logic"
-                  "/kairos-prover-server-lib"
-                  "/Cargo.toml"
-                ]);
-              };
-          sourceRoot = "source/kairos-prover";
-          cargoLock = {
-            lockFile = ./Cargo.lock;
-            allowBuiltinFetchGit = true;
-          };
-          nativeBuildInputs = [ pkgs.makeWrapper ];
-          postInstall = ''
-            wrapProgram $out/bin/kairos-prover-risc0-server \
-              --set PATH ${pkgs.lib.makeBinPath [ inputs'.risc0pkgs.packages.r0vm ]}
-          '';
-        };
+        kairos-prover-deps = craneLib.buildDepsOnly (kairosProverAttrs // {
+          pname = "kairos";
+        });
+
+        kairos-prover = craneLib.buildPackage (kairosProverAttrs // {
+          cargoArtifacts = self'.packages.kairos-prover-deps;
+          meta.mainProgram = "kairos-prover";
+        });
       };
     };
   flake = { };
