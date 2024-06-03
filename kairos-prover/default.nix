3b21e44f46942b2257b6e4e47376c969b8b16b0b
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-06-03 12:49:22 +0200

    kairos-prover: only enable dev mode when testing

--- kairos-prover/default.nix
+++ kairos-prover/default.nix
@@ -46,9 +46,11 @@
           ];
         };
 
-        # Proving in CI is disabled because it takes too long.
-        # Proving is a test of risc0, not kairos anyway.
-        RISC0_DEV_MODE = 1;
+        preCheck = ''
+          # Proving in CI is disabled because it takes too long.
+          # Proving is a test of risc0, not kairos anyway.
+          export RISC0_DEV_MODE=1;
+        '';
         preBuild = ''
           # The vendored cargo sources will be placed into .cargo-home,
           # however it seems that since the risc0_build crate
