78162b9066f3b4ce64979dc82f6c3074b34ece0d
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-06-07 14:02:52 +0200

    kairos-test-utils/cctl: implement and test deploy_contract function

--- kairos-test-utils/bin/cctld.rs
+++ kairos-test-utils/bin/cctld.rs
@@ -4,16 +4,30 @@ use sd_notify::NotifyState;
 use std::path::PathBuf;
 use tokio::signal;
 
+use crate::cctl::DeployableContract;
+
 #[derive(Parser)]
 pub struct Cli {
     #[arg(short, long)]
     pub working_dir: Option<PathBuf>,
+    #[arg(short, long, num_args(0..))]
+    pub deploy_contract: Option<String>,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
-    let _network = cctl::CCTLNetwork::run(cli.working_dir)
+    let deploy_contract =
+        cli.deploy_contract.map(|deploy_contracts_arg| {
+            match deploy_contracts_arg.split_once(':') {
+                Some((hash_name, path)) => DeployableContract {
+                    hash_name: hash_name.to_string(),
+                    path: PathBuf::from(&path),
+                },
+                None => panic!("Error parsing the provided deploy contracts argument."),
+            }
+        });
+    let _network = cctl::CCTLNetwork::run(cli.working_dir, deploy_contract)
         .await
         .expect("An error occured while starting the CCTL network");
 
