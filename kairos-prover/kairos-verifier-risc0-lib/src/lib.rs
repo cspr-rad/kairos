2d9aa2a356439968b8898ac28c5faf1ea1b308b3
Avi Dessauer <avi.the.coder@gmail.com>
2024-06-12 17:29:29 -0500

    Hardcode the batch circuit hash

--- kairos-prover/kairos-verifier-risc0-lib/src/lib.rs
+++ kairos-prover/kairos-verifier-risc0-lib/src/lib.rs
@@ -1,18 +1,27 @@
 #![no_std]
-extern crate alloc;
 
-use alloc::{format, string::String};
+/// The risc0 image_id of the batch update circuit program.
+pub const BATCH_CIRCUIT_PROGRAM_HASH: [u32; 8] = [
+    1854829392, 1056154338, 1061691258, 418817515, 2587812152, 317072623, 3636186866, 3117322684,
+];
 
-use kairos_circuit_logic::ProofOutputs;
+#[cfg(feature = "verifier")]
+pub mod verifier {
+    extern crate alloc;
 
-use risc0_zkvm::Receipt;
+    use alloc::{format, string::String};
 
-pub fn verify_execution(receipt: &Receipt, program_id: [u32; 8]) -> Result<ProofOutputs, String> {
-    receipt
-        .verify(program_id)
-        .map_err(|e| format!("Error in risc0_zkvm verify: {e}"))?;
+    use kairos_circuit_logic::ProofOutputs;
 
-    let proof_outputs = ProofOutputs::rkyv_deserialize(&receipt.journal.bytes)?;
+    use risc0_zkvm::Receipt;
 
-    Ok(proof_outputs)
+    pub fn verify_execution(receipt: &Receipt) -> Result<ProofOutputs, String> {
+        receipt
+            .verify(crate::BATCH_CIRCUIT_PROGRAM_HASH)
+            .map_err(|e| format!("Error in risc0_zkvm verify: {e}"))?;
+
+        let proof_outputs = ProofOutputs::rkyv_deserialize(&receipt.journal.bytes)?;
+
+        Ok(proof_outputs)
+    }
 }
--- kairos-prover/methods/build.rs
+++ kairos-prover/methods/build.rs
@@ -2,33 +2,14 @@ fn main() {
     let guests = risc0_build::embed_methods();
     match guests.as_slice() {
         [guest] => {
-            let program_id_bytes: Vec<u8> = guest
-                .image_id
-                .iter()
-                .flat_map(|x| x.to_le_bytes())
-                .collect();
+            let hardcoded_id = kairos_verifier_risc0_lib::BATCH_CIRCUIT_PROGRAM_HASH;
 
-            let hexed_program_id = hex::encode(program_id_bytes);
-
-            let mut env_id = std::env::var("KAIROS_BATCH_CIRCUIT_PROGRAM_HASH");
-
-            // If the env var is not set (by nix), try to load it from .env
-            if env_id.is_err() {
-                let _ = dotenvy::dotenv();
-                env_id = std::env::var("KAIROS_BATCH_CIRCUIT_PROGRAM_HASH");
-            }
-
-            match env_id {
-                Ok(env_hash) if env_hash == hexed_program_id => {}
-                Ok(env_hash) => {
-                    panic!("`KAIROS_BATCH_CIRCUIT_PROGRAM_HASH={env_hash}` is out of date.\n\
-                            set `KAIROS_BATCH_CIRCUIT_PROGRAM_HASH=\"{hexed_program_id}\"` in nix and .env");
-                }
-                Err(e) => {
-                    panic!("`KAIROS_BATCH_CIRCUIT_PROGRAM_HASH` is not set: {e}\n\
-                            set `KAIROS_BATCH_CIRCUIT_PROGRAM_HASH=\"{hexed_program_id}\"` in nix and .env");
-                }
-            }
+            assert_eq!(
+                hardcoded_id,
+                guest.image_id,
+                "`kairos_verifier_risc0_lib::BATCH_CIRCUIT_PROGRAM_HASH = {hardcoded_id:?}` is out of date.\n\
+                 Update the hardcoded value to the new hash: {0:?}", guest.image_id
+            );
         }
         _ => panic!("Expected exactly one guest method"),
     }
