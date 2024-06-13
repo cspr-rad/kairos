0cdef5b124235f057383dc22e141f3d24b541801
Andrzej Broński <andrzej1_1@o2.pl>
2024-06-13 14:41:25 +0200

    Attempt to use crypto module with `no_std` compatibility.

--- kairos-contracts/demo-contract/contract/src/utils.rs
+++ kairos-contracts/demo-contract/contract/src/utils.rs
@@ -5,8 +5,6 @@ use casper_contract::contract_api::runtime;
 use casper_types::bytesrepr::FromBytes;
 use casper_types::{account::AccountHash, system::CallStackElement, Key, U512};
 
-use kairos_crypto::CryptoSigner;
-
 pub mod errors;
 use errors::DepositError;
 
@@ -73,7 +71,7 @@ pub(crate) fn validate_deposit_tx(tx_bytes: &[u8], amount: &U512) -> Result<(), 
     }
 
     // Signature must be valid.
-    let signer = kairos_crypto::implementations::Signer::from_public_key(tx_raw_public_key)
+    let signer = kairos_crypto_tmp::implementations::Signer::from_public_key(tx_raw_public_key)
         .map_err(|_e| DepositError::FailedToCreateSigner)?;
     signer
         .verify_tx(tx)
