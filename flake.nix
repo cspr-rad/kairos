eec2325debeeecf22d5ff21684cca28be896138d
Andrzej Broński <andrzej1_1@o2.pl>
2024-06-24 12:11:27 +0200

    Attempt to fix Nix build.

--- flake.nix
+++ flake.nix
@@ -66,6 +66,13 @@
             cargoExtraArgs = "--target wasm32-unknown-unknown";
             CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_LINKER = "lld";
             nativeBuildInputs = [ pkgs.binaryen pkgs.lld pkgs.llvmPackages.bintools ];
+            buildInputs = with pkgs; [
+              openssl.dev
+            ] ++ lib.optionals stdenv.isDarwin [
+              libiconv
+              darwin.apple_sdk.frameworks.Security
+              darwin.apple_sdk.frameworks.SystemConfiguration
+            ];
             doCheck = false;
             # Append "-optimized" to wasm files, to make the tests pass
             postInstall = ''
