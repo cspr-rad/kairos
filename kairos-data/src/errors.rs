843f84f4abc299e4ab94e56287aba850290e945e
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-07-11 17:31:10 +0200

    kairos-data: make errors more verbose

--- kairos-data/src/errors.rs
+++ kairos-data/src/errors.rs
@@ -5,12 +5,12 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum DBError {
-    #[error("There was an issue connecting to the database")]
+    #[error("There was an issue connecting to the database: {0}")]
     ConnectionError(#[from] DDError),
-    #[error("There was an error in sync code executing in a separate thread")]
+    #[error("There was an error in sync code executing in a separate thread: {0}")]
     InteractError(#[from] DDIError),
-    #[error("Pool error")]
+    #[error("Pool error: {0}")]
     PoolError(#[from] deadpool::managed::PoolError<deadpool_diesel::Error>),
-    #[error("Diesel error")]
+    #[error("Diesel error: {0}")]
     DieselError(#[from] DieselError),
 }
