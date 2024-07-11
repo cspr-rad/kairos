634dabc6f8a561fa5276270acdadd9187d92aa18
Andrzej Broński <andrzej1_1@o2.pl>
2024-07-11 12:06:58 +0200

    Add tests for run failure.

--- casper-deploy-notifier/tests/casper-deploy-notifier.rs
+++ casper-deploy-notifier/tests/casper-deploy-notifier.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod tests {
     use casper_deploy_notifier::*;
+    use tokio::sync::mpsc;
 
     #[tokio::test]
     async fn test_connection_failure() {
@@ -55,5 +56,74 @@ mod tests {
         let mut notifier = DeployNotifier::new(&server.url());
         let result = notifier.connect().await;
         assert!(matches!(result, Err(SseError::InvalidHandshake)));
+    }
+
+    #[tokio::test]
+    async fn test_run_without_connection() {
+        let (tx, _rx) = mpsc::channel(1);
+        let mut notifier = DeployNotifier::default();
+        let result = notifier.run(tx).await;
+
+        assert!(matches!(result, Err(SseError::NotConnected)));
+    }
+
+    #[tokio::test]
+    async fn test_handling_node_shutdown() {
+        let mut server = mockito::Server::new_async().await;
+        server
+            .mock("GET", "/")
+            .with_header("content-type", "text/event-stream")
+            .with_body(concat!(
+                "data: {\"ApiVersion\": \"1.5.6\"}\n\n",
+                "data: \"Shutdown\"\n\n"
+            ))
+            .create_async()
+            .await;
+
+        let mut notifier = DeployNotifier::new(&server.url());
+        notifier.connect().await.unwrap();
+
+        let (tx, _rx) = mpsc::channel(1);
+        let result = notifier.run(tx).await;
+        assert!(matches!(result, Err(SseError::NodeShutdown)));
+    }
+
+    #[tokio::test]
+    async fn test_run_stream_exhausted() {
+        let mut server = mockito::Server::new_async().await;
+        server
+            .mock("GET", "/")
+            .with_header("content-type", "text/event-stream")
+            .with_body("data: {\"ApiVersion\": \"1.5.6\"}\n\n")
+            .create_async()
+            .await;
+
+        let mut notifier = DeployNotifier::new(&server.url());
+        notifier.connect().await.unwrap();
+
+        let (tx, _rx) = mpsc::channel(1);
+        let result = notifier.run(tx).await;
+        assert!(matches!(result, Err(SseError::StreamExhausted)));
+    }
+
+    #[tokio::test]
+    async fn test_unexpected_handshake() {
+        let mut server = mockito::Server::new_async().await;
+        server
+            .mock("GET", "/")
+            .with_header("content-type", "text/event-stream")
+            .with_body(concat!(
+                "data: {\"ApiVersion\": \"1.5.6\"}\n\n",
+                "data: {\"ApiVersion\": \"1.5.6\"}\n\n"
+            ))
+            .create_async()
+            .await;
+
+        let mut notifier = DeployNotifier::new(&server.url());
+        notifier.connect().await.unwrap();
+
+        let (tx, _rx) = mpsc::channel(1);
+        let result = notifier.run(tx).await;
+        assert!(matches!(result, Err(SseError::UnexpectedHandshake)));
     }
 }
