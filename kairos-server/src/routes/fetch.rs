87b0dc8ca10f3137bd0bdc829e354c01bcd23db7
Marijan Petričević <marijan.petricevic94@gmail.com>
2024-07-23 10:28:22 +0200

    kairos-server/fetch: simplify

--- kairos-server/src/routes/fetch.rs
+++ kairos-server/src/routes/fetch.rs
@@ -1,21 +1,9 @@
 use axum::{extract::State, Json};
 use axum_extra::routing::TypedPath;
-use chrono::NaiveDateTime;
 use kairos_data::transaction::*;
-use serde::Deserialize;
 use tracing::instrument;
 
 use crate::{state::ServerState, AppErr};
-
-#[derive(Deserialize, Debug)]
-pub struct QueryTransactionsPayload {
-    sender: Option<String>,
-    min_timestamp: Option<String>,
-    max_timestamp: Option<String>,
-    min_amount: Option<i64>,
-    max_amount: Option<i64>,
-    recipient: Option<String>,
-}
 
 #[derive(TypedPath)]
 #[typed_path("/api/v1/transactions")]
@@ -25,22 +13,7 @@ pub struct QueryTransactionsPath;
 pub async fn query_transactions_handler(
     _: QueryTransactionsPath,
     State(state): State<ServerState>,
-    Json(payload): Json<QueryTransactionsPayload>,
+    Json(filter): Json<TransactionFilter>,
 ) -> Result<Json<Vec<Transactions>>, AppErr> {
-    let filter = TransactionFilter {
-        sender: payload.sender,
-        min_timestamp: payload
-            .min_timestamp
-            .and_then(|ts| NaiveDateTime::parse_from_str(&ts, "%Y-%m-%d %H:%M:%S").ok()),
-        max_timestamp: payload
-            .max_timestamp
-            .and_then(|ts| NaiveDateTime::parse_from_str(&ts, "%Y-%m-%d %H:%M:%S").ok()),
-        min_amount: payload.min_amount,
-        max_amount: payload.max_amount,
-        recipient: payload.recipient,
-    };
-
-    let transactions = get(&state.pool, filter).await?;
-
-    Ok(Json(transactions))
+    get(&state.pool, filter).await.map(Json).map_err(Into::into)
 }
