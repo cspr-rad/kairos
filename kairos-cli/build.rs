929c373f48b0f01623711535ea51651a73d17e22
Andrzej Broński <andrzej1_1@o2.pl>
2024-07-25 10:34:46 +0200

    CLI: Automatic WASM discovery and optimization.
    `PATH_TO_SESSION_BINARIES` no longer needs to be set :).

--- kairos-cli/build.rs
+++ kairos-cli/build.rs
@@ -1,11 +1,10 @@
 use std::env;
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 fn main() {
     // Path
-    let session_binaries_dir = env::var("PATH_TO_SESSION_BINARIES")
-        .expect("PATH_TO_SESSION_BINARIES environment variable is not set");
+    let session_binaries_dir = get_wasm_directory();
 
     // Get the output directory set by Cargo.
     let out_dir = env::var("OUT_DIR").expect("OUT_DIR not set");
@@ -17,4 +16,69 @@ fn main() {
 
     // Print out a message to re-run this script if the source file changes.
     println!("cargo:rerun-if-changed={}", source_path.display());
+}
+
+pub fn get_wasm_directory() -> PathBuf {
+    // Environment variable, or default path based on the project structure.
+    let base_path_session = if let Ok(custom_path) = env::var("PATH_TO_SESSION_BINARIES") {
+        PathBuf::from(custom_path)
+    } else {
+        let project_root = env::var("CARGO_MANIFEST_DIR").unwrap_or_else(|_| ".".to_string());
+        PathBuf::from(project_root)
+            .join("../kairos-session-code/target/wasm32-unknown-unknown/release/")
+    };
+
+    if !base_path_session.exists() {
+        panic!("WASM directory does not exist: {}. Please build session code at `./kairos-session-code` with `cargo build --release`; or set `PATH_TO_SESSION_BINARIES` env variable.", base_path_session.display());
+    }
+
+    // Ensure all WASM files are optimized.
+    optimize_files(&base_path_session).expect("Unable to optimize WASM files");
+
+    base_path_session
+}
+
+fn optimize_files(dir: &Path) -> Result<(), String> {
+    let entries = fs::read_dir(dir).map_err(|e| e.to_string())?;
+
+    let mut found_wasm = false;
+    for entry in entries {
+        let entry = entry.map_err(|e| e.to_string())?;
+        let path = entry.path();
+        if path.extension().and_then(|s| s.to_str()) == Some("wasm") {
+            found_wasm = true;
+
+            // Skip already optimized files.
+            let file_name = path.file_name().unwrap().to_str().unwrap();
+            if file_name.ends_with("-optimized.wasm") {
+                continue;
+            }
+
+            // Skip if optimized file already exists.
+            let optimized_file_name = format!(
+                "{}-optimized.wasm",
+                file_name.strip_suffix(".wasm").unwrap()
+            );
+            let optimized_file_path = dir.join(&optimized_file_name);
+            if optimized_file_path.exists() {
+                continue;
+            }
+
+            // Optimize and save as new file.
+            let infile = path.to_str().unwrap().to_string();
+            let outfile = optimized_file_path.to_str().unwrap().to_string();
+
+            let mut opts = wasm_opt::OptimizationOptions::new_optimize_for_size();
+            opts.add_pass(wasm_opt::Pass::StripDebug);
+            opts.add_pass(wasm_opt::Pass::SignextLowering);
+
+            opts.run(&infile, &outfile).map_err(|e| e.to_string())?;
+        }
+    }
+
+    if !found_wasm {
+        return Err("No WASM files found in the directory. You should change directory to `./kairos-session-code` and build with `cargo build --release`.".to_string());
+    }
+
+    Ok(())
 }
